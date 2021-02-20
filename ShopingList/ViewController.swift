15b9f4983c231ed6922c56137e90bce7bab1c340
Вадим <vadim_ponomarev_1989@mail.ru>
2021-02-21 00:16:04 +0300

    Initial Commit

---
+++ ShopingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShopingList
+//
+//  Created by Вадим on 21.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
