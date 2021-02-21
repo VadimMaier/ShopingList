810e3413ff9692dcee993402609d2678414c6b54
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-02-22 01:04:19 +0300

    Updated main Model and DataManager

---
+++ ShopingList/Models/Good.swift
@@ -0,0 +1,29 @@
+//
+//  Notes.swift
+//  ShopingList
+//
+//  Created by Aleksei Goranov on 21.02.2021.
+//
+
+import Foundation
+
+struct Good {
+    let name: String
+    let count: Int
+}
+
+extension Good {
+    static func getNotes() -> [Good] {
+        
+        var newGoods: [Good] = []
+        
+        let goods = GoodsDataManager.shared.goods
+        
+        for good in goods {
+            newGoods.append(Good(name: good.key,
+                                 count: good.value))
+        }
+        
+        return newGoods
+    }
+}
---
+++ ShopingList/Services/GoodsDataManager.swift
@@ -0,0 +1,27 @@
+//
+//  NotesDataManager.swift
+//  ShopingList
+//
+//  Created by Aleksei Goranov on 21.02.2021.
+//
+
+import Foundation
+
+
+class GoodsDataManager {
+    
+    static let shared = GoodsDataManager()
+    
+    var goods = [
+        "Молоко" : 2,
+        "хлеб" : 1,
+        "печенье" : 3,
+        "говядина" : 1,
+        "мороженое" : 4,
+        "фери" : 1
+    ]
+    
+    private init() {}
+    
+    
+}
